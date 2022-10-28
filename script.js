8f8c42eab51b31b6fbb5b730498bbc5b1b03c8fc
MarcosGerminiodeSouza <marcos.germiniodesouza@gmail.com>
2022-10-28 15:07:23 -0300

    Aula04_JS

--- script.js
+++ script.js
@@ -7,7 +7,10 @@ snake[0] = {
     y: 8 * box
 }
 let direction = "right";
-
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
 function criarBG(){
     context.fillStyle = "lightgreen";
     context.fillRect(0, 0, 16 * box, 16 * box);
@@ -18,6 +21,11 @@ function criarCobrinha(){
         context.fillStyle = "green";
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
+}
+
+function drawFood(){
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box);
 }
 
 document.addEventListener('keydown', update);
@@ -37,6 +45,7 @@ function iniciarJogo(){
 
     criarBG();
     criarCobrinha();
+    drawFood();
 
     let snakeX = [0].x;
     let snakeY = [0].y;
